1c01ed51602a545afbb33708d3aa4982090747b2
Jeyaprakash <jeyaprakash.rajagopal@gmail.com>
2025-09-04 20:40:43 +0200

    removing main duplicate file

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from langchain-projects!")
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from schemas import AgentResponse
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4o-mini")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"]
+).partial(format_instructions=output_parser.get_format_instructions())
+
+# returns runnable and this is a chain
+agent = create_react_agent(
+    llm = llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
+
+def main():
+    print("Reach Search Agent!")
+    result = chain.invoke(input={
+        "input": "Search for 3 job postings for an AI engineer using langchain in bayarea strictly on linkedin and list their details",
+    })
+    print(result)
 
 if __name__ == "__main__":
     main()
