55a552af01f75ea6c4eb608ab396265389e41b91
Jeyaprakash <jeyaprakash.rajagopal@gmail.com>
2026-01-26 10:53:39 +0100

    reflection agent final code

--- 06_langgraph_reflection_agent/chains.py
+++ 06_langgraph_reflection_agent/chains.py
@@ -1,24 +1,33 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 reflection_prompt_template = ChatPromptTemplate.from_messages(
-    ('system',
-     "You are a viral twitter influencer who is going to grade the user's tweet. Generate critique and recommendations for the user tweet.",
+    [('system',
+     "You are a viral twitter influencer who is going to grade the user's tweet. Generate critique and recommendations for the user tweet."
      "Always provide the detailled recommendations, including requests for length, virality, style, etc."
      ),
-     MessagesPlaceholder(variable_name="messages")
+     MessagesPlaceholder(variable_name="messages"),
+     ]
 )
 
 generation_prompt_template = ChatPromptTemplate.from_messages(
-    ('system',
-     "You are a best twitter tweets writter who specializes in creating viral posts.",
+    [('system',
+     "You are a best twitter tweets writter who specializes in creating viral posts."
      "Generate the best twitter posts possible with user's request."
      "You should provide improved version of previous attempts of the twitter tweet based on the user critique and the recommendations."
      ),
-     MessagesPlaceholder(variable_name="messages")
+     MessagesPlaceholder(variable_name="messages"),
+     ]     
 )
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    max_tokens=None
+)
 
 reflection_chain = reflection_prompt_template | llm
 generation_chain = generation_prompt_template | llm
--- 06_langgraph_reflection_agent/main.py
+++ 06_langgraph_reflection_agent/main.py
@@ -1,6 +1,55 @@
+from typing import TypedDict, Annotated
+from langgraph.graph import add_messages, StateGraph, END
+from langchain_core.messages import BaseMessage, HumanMessage
 from dotenv import load_dotenv
+from chains import generation_chain, reflection_chain
 
 load_dotenv()
 
+class MessagesGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def should_continue(state: MessagesGraph) -> str:
+    if len(state["messages"]) > 6:
+        return END
+    return REFLECT
+
+REFLECT = "reflect"
+GENERATE = "generate"
+LAST = -1
+
+def generation_node(state: MessagesGraph):
+    return {"messages": [generation_chain.invoke({"messages": state["messages"]})]}
+
+def reflection_node(state: MessagesGraph):
+    result = reflection_chain.invoke({"messages": state["messages"]})
+
+    return {"messages": [HumanMessage(content=result.content)]}
+
+flow = StateGraph(MessagesGraph)
+flow.add_node(GENERATE, generation_node)
+flow.add_node(REFLECT, reflection_node)
+
+flow.set_entry_point(GENERATE)
+
+flow.add_conditional_edges(GENERATE, should_continue, path_map={
+    END:END,
+    REFLECT:REFLECT
+})
+
+flow.add_edge(REFLECT, GENERATE)
+
+app = flow.compile()
+app.get_graph().draw_mermaid_png(output_file_path="reflection.png")
+
 if __name__ == "__main__":
-    print("Reflection agent!")
+    print("Reflection agent")
+    result = app.invoke({"messages": [HumanMessage(
+        content="""Make this tweet better:" 
+        @LangChainAI
+        — newly Tool Calling feature is seriously underrated.
+        After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+        Made a video covering their newest blog post
+        """
+    )]})
+    print(result)
